e239657a40d3f91406d69d34bbfa20f80b5afa9c
sebastian tran <transebastian21@gmail.com>
2025-12-09 11:34:42 -0500

    Initial Commit

---
+++ tran_sebastian_Lab05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tran_sebastian_Lab05
+//
+//  Created by Tran, Sebastian on 12/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
